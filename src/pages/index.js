1dd5466ad299ac195b9781859d016c57e38c70c3
Nick-vr <nvanroyen@gmail.com>
2019-03-08 00:01:43 +0100

    Dotdot blink

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,37 +3,43 @@ import styled, { keyframes } from 'styled-components'
 
 import Layout from '../components/layout'
 
+const Wrap = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+
+  h1 {
+    margin-bottom: -20px;
+  }
+`
+
 const blink = keyframes`
   0%{background-color: #FFF;}
   49%{background-color: #000;}
   50%{background-color: #000;}
+  90%{background-color: #FFF;}
   99%{background-color: #000;}
   100%{background-color: #FFF;}
 `
 
 const Dot = styled.div`
-  width: 30px;
-  height: 30px;
   animation: ${blink} 1.7s infinite ease;
-  margin: 0 0 130px 30px;
-`
-
-const InLine = styled.div`
-  display: flex;
-  align-items: flex-end;
-
-  h1 {
-  }
+  align-self: center;
+  width: 20px;
+  height: 20px;
+  margin-top: 100px;
 `
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi</h1>
-    <h1>I'm</h1>
-    <InLine>
+    <Wrap>
+      <Dot />
+      <h1>Hi</h1>
+      <h1>I'm</h1>
       <h1>Nick</h1>
       <Dot />
-    </InLine>
+    </Wrap>
   </Layout>
 )
 
